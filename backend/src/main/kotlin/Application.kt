7bc0fabe868549db50c9f253f8f4ea733ecd165d
Simon Vergauwen <nomisRev@users.noreply.github.com>
2025-04-28 16:06:55 +0200

    Refactor tests and introduce Customer data model
    Reorganized test structure, introduced a `Customer` data model, and shifted tests to use `runBlocking` for better coroutine handling. Added placeholders for CRUD operations on customer data to extend functionality.

--- backend/src/main/kotlin/Application.kt
+++ backend/src/main/kotlin/Application.kt
@@ -9,6 +9,8 @@ import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
@@ -23,3 +25,6 @@ fun Application.module() {
         }
     }
 }
+
+@Serializable
+data class Customer(val id: Int, val name: String, val email: String, val createdAt: Instant)
--- backend/src/test/kotlin/ApplicationTest.kt
+++ backend/src/test/kotlin/ApplicationTest.kt
@@ -3,33 +3,68 @@ package org.jetbrains
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.*
-import io.ktor.client.statement.bodyAsText
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.testing.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.Clock
+import org.junit.AfterClass
+import org.junit.ClassRule
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ApplicationTest {
-
     @Test
-    fun testRoot() = testApplication {
-        application {
-            module()
-        }
+    fun testRoot() = runBlocking {
         val response = client.get("/")
         assert(response.status == HttpStatusCode.OK)
     }
 
     @Test
-    fun testJson() = testApplication {
-        application {
-            module()
-        }
-        val response = createClient {
-            install(ContentNegotiation) { json() }
-        }.get("/json")
+    fun testJson() = runBlocking {
+        val response = client.get("/json")
         assert(response.status == HttpStatusCode.OK)
         assert(response.body<Map<String, String>>() == mapOf("hello" to "world"))
+    }
+
+    val fakeData = mutableListOf(
+        Customer(1, "Anton", "anton@jb.com", Clock.System.now()),
+        Customer(2, "Leonid", "leonid@jb.com", Clock.System.now()),
+        Customer(3, "Simon", "simon@jb.com", Clock.System.now()),
+    )
+
+    @Test
+    fun `get all data`(): Unit = runBlocking {
+
+    }
+
+    @Test
+    fun `post data instance`(): Unit = runBlocking {
+
+    }
+
+    @Test
+    fun `put data instance`(): Unit = runBlocking {
+
+    }
+
+    @Test
+    fun `delete data instance`(): Unit = runBlocking {
+
+    }
+
+    companion object {
+        val app = TestApplication {
+            application { module() }
+        }
+
+        val client = app.createClient {
+            install(ContentNegotiation) { json() }
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun close() = runBlocking {
+            app.stop()
+        }
     }
 }
